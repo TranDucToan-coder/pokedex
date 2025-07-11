383fcd5c36d7d016d977d6377e9ce8ea826e0244
KURO <ductoantran17@gmail.com>
2025-07-11 11:06:05 +0700

    Verify OTP

--- Routes/LoginRoutes.js
+++ Routes/LoginRoutes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const LoginController = require("../Controller/UserController")
 const MiddlewareController = require("../Controller/Middleware")
+const otpController = require("../Controller/SendOTP")
 const route = express.Router();
 
 route.get("/:id", LoginController.viewUser)
 route.post("/login", LoginController.detailUser)
 route.post("/insert", MiddlewareController.middleware, LoginController.addUser)
 route.put("/updateprofile/:id",LoginController.updateUser)
-route.put(".updatepassword/:id", LoginController.updatePassword)
+route.put("/updatepassword/:id", LoginController.updatePassword)
+
+route.post("/sendOTP", otpController.CreateOTP)
+route.post("/verifyOTP", otpController.VerifyOTP)
 
 module.exports = route
